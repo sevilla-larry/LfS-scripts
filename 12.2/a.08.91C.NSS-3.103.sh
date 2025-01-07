3232f1d4cc9e7277490cd84ec8cb08927fcdb0e0
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-01-07 18:34:37 +0800

    log bug fix

--- 12.2/a.08.91C.NSS-3.103.sh
+++ 12.2/a.08.91C.NSS-3.103.sh
@@ -2,7 +2,7 @@
 #
 
 export PKG="nss-3.103"
-export PKGLOG_DIR=$LFSLOG/08.91B
+export PKGLOG_DIR=$LFSLOG/08.91C
 export PKGLOG_TAR=$PKGLOG_DIR/tar.log
 #export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
