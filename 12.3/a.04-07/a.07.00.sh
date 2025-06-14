54a02ba6506be0a160ffecbbd86157c10a2b5536
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-06-14 21:03:26 +0800

    fix perl version

--- 12.3/a.04-07/a.07.00.sh
+++ 12.3/a.04-07/a.07.00.sh
@@ -36,10 +36,10 @@ echo "."
 
 ###
 
-echo "07.09.Perl-5.40.1"
-echo "07.09.Perl-5.40.1" >> $LFSLOG_PROCESS
+echo "07.09.Perl-5.40.2"
+echo "07.09.Perl-5.40.2" >> $LFSLOG_PROCESS
 
-time { bash a.07.09.Perl-5.40.1.sh ; }
+time { bash a.07.09.Perl-5.40.2.sh ; }
 
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
